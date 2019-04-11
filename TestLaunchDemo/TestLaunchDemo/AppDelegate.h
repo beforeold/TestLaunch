3c518540a5ee02c8cf5fa456f7dd7448cfc65513
席萍萍 Brook <cirpxpp@163.com>
2019-04-11 13:43:57 +0800

    add project

---
+++ TestLaunchDemo/TestLaunchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLaunchDemo
+//
+//  Created by Brook on 2019/4/11.
+//  Copyright © 2019 br. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
